ab67e02c288332927c0cce021b69d1caeff2de07
Wake <georgerincon2@gmail.com>
2019-01-05 22:18:27 -0500

    mmm

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const exphbr = require('express-handlebars');
 const path = require('path');
 const routing = require('./routes/apiRoutes');
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('handlebars', exphbr({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routing);
