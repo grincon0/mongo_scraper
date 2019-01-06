446eda8979a8a0dfba723de0a389649788c940a6
Wake <georgerincon2@gmail.com>
2019-01-05 19:45:57 -0500

    bugfix

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const axios = require('axios');
 
-const db = require('../models/index');
+const db = require('../models/Index');
 
 const cheerio = require('cheerio');
 
